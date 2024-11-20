9cf1f3135be1c37365b873a4a8da4bd80fc54bdb
aidansterling6 <95391945+aidansterling6@users.noreply.github.com>
2024-11-20 15:35:32 -0800

    update to plants

--- Game/entities/plants/Flower/flower.gd
+++ Game/entities/plants/Flower/flower.gd
@@ -1,10 +1,18 @@
 extends Plant
 class_name Flower
 
-func Grow(sun: float, water: float)->void:
+func grow()->void:
+	#if sun > value and water > value
+	#	Reduce water by value
+	#	Move to next stage
+	#else if sun < value2 or water < value2
+	#	if !Wither
+	#		Wither
+	#	else
+	#		Die
 	pass
 
-static func CreatePlant(Pos: Vector2i, StageIndex: int)->Plant:
+static func create_plant(Pos: Vector2i, StageIndex: int)->Plant:
 	var out = Plant.new();
 	out._Pos = Pos;
 	out._StageIndex = StageIndex;
--- Game/entities/plants/Plant/plant.gd
+++ Game/entities/plants/Plant/plant.gd
@@ -1,27 +1,17 @@
-extends Node2D
+#extends Node2D
 class_name Plant
 
-@export var scene: Node2D;
-@export var stages: Array[Sprite2D];
 var _StageIndex: int;
 var _Pos: Vector2i;
 
-func Grow(sun: float, water: float)->void:
+func grow()->void:
 	pass
 
 #set a plants growth stage
-func SetStage(StageIndex: int)->bool:
-	if StageIndex not in range(0, stages.size()):
-		return false;
-	_StageIndex = StageIndex;
-	for i in range(0, stages.size()):
-		if i == StageIndex:
-			stages[i].show();
-		else:
-			stages[i].hide();
-	return true;
+func set_stage(StageIndex: int)->bool:#needs to be able to set tiles via map
+	return false;
 
 #create a new plant: call this function on the subclass you want, not Plant
-static func CreatePlant(Pos: Vector2i, StageIndex: int)->Plant:
+static func create_plant(Pos: Vector2i, StageIndex: int)->Plant:
 	assert(false, "call createPlant on a subclass of plant, not the base class");
 	return null;
