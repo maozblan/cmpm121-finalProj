a2547e0d20ff2774f5e4d6853dc9b54c864691ce
aidansterling6 <95391945+aidansterling6@users.noreply.github.com>
2024-11-19 20:35:09 -0800

    created base plant class and removed example plant

---
+++ Game/entities/plants/Flower/flower.gd
@@ -0,0 +1,5 @@
+extends Plant
+class_name Flower
+
+func grow(sun: float, water: float)->void:
+	pass
--- Game/entities/plants/Plant/plant.gd
+++ Game/entities/plants/Plant/plant.gd
@@ -1,26 +1,9 @@
 extends Node2D
 class_name Plant
 
-enum PLANT_TYPE {
-	FLOWER
-}
-class Point:
-	var x: int = 0;
-	var y: int = 0;
+@export var stages: Array[Sprite2D];
+var stage: int;
+var pos: Vector2i;
 
-var type: PLANT_TYPE = PLANT_TYPE.FLOWER;
-var pos: Point;
-
-
-# Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
-
-
-func update() ->void:
+func grow(sun: float, water: float)->void:
 	pass
