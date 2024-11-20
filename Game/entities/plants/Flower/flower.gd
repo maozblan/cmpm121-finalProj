aa24135bef5ab0120b328b1805bb5dd4e24ce096
aidansterling6 <95391945+aidansterling6@users.noreply.github.com>
2024-11-19 21:34:36 -0800

    updated plants with a CreatePlant function

--- Game/entities/plants/Flower/flower.gd
+++ Game/entities/plants/Flower/flower.gd
@@ -1,5 +1,15 @@
 extends Plant
 class_name Flower
 
-func grow(sun: float, water: float)->void:
+func Grow(sun: float, water: float)->void:
 	pass
+
+static func createPlant(Pos: Vector2i, StageIndex: int)->Plant:
+	var out = Plant.new();
+	out._Pos = Pos;
+	out._StageIndex = StageIndex;
+	if not out.SetStage(StageIndex):
+		out.hide();
+		out.queue_free();
+		return null;
+	return out;
--- Game/entities/plants/Plant/plant.gd
+++ Game/entities/plants/Plant/plant.gd
@@ -1,9 +1,27 @@
 extends Node2D
 class_name Plant
 
+@export var scene: Node2D;
 @export var stages: Array[Sprite2D];
-var stage: int;
-var pos: Vector2i;
+var _StageIndex: int;
+var _Pos: Vector2i;
 
-func grow(sun: float, water: float)->void:
+func Grow(sun: float, water: float)->void:
 	pass
+
+#set a plants growth stage
+func SetStage(StageIndex: int)->bool:
+	if StageIndex not in range(0, stages.size()):
+		return false;
+	_StageIndex = StageIndex;
+	for i in range(0, stages.size()):
+		if i == StageIndex:
+			stages[i].show();
+		else:
+			stages[i].hide();
+	return true;
+
+#create a new plant: call this function on the subclass you want, not Plant
+static func createPlant(Pos: Vector2i, StageIndex: int)->Plant:
+	assert(false, "call createPlant on a subclass of plant, not the base class");
+	return null;
