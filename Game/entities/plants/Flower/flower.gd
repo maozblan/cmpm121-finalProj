c64224b459dd40755f8b077ba5f5337ea57db28f
aidansterling6 <95391945+aidansterling6@users.noreply.github.com>
2024-11-21 10:33:49 -0800

    small plant update

--- Game/entities/plants/Flower/flower.gd
+++ Game/entities/plants/Flower/flower.gd
@@ -2,6 +2,11 @@ extends Plant
 class_name Flower
 
 func grow()->void:
+	var tmp_cell: tmp_cell_class = _Map.get_cell(Vector2i.ZERO);
+	if tmp_cell.sun > 3 and tmp_cell.water > 3:
+		_SetStage(_StageIndex + 1);
+	elif tmp_cell.sun < 1 or tmp_cell.water < 1:
+		pass
 	#if sun > value and water > value
 	#	Reduce water by value
 	#	Move to next stage
@@ -12,10 +17,11 @@ func grow()->void:
 	#		Die
 	pass
 
-static func create_plant(Pos: Vector2i, StageIndex: int)->Plant:
+static func create_plant(Pos: Vector2i, Map: tmp_map_class, StageIndex: int)->Plant:
 	var out = Plant.new();
 	out._Pos = Pos;
 	out._StageIndex = StageIndex;
+	out._Map = Map;
 	if not out.SetStage(StageIndex):
 		out.hide();
 		out.queue_free();
--- Game/entities/plants/Plant/plant.gd
+++ Game/entities/plants/Plant/plant.gd
@@ -1,17 +1,36 @@
 #extends Node2D
 class_name Plant
 
+class tmp_cell_class:
+	var sunlight: int;
+	var water: int;
+	var plant: Plant;
+
+class tmp_map_class:
+	func get_cell(cord:Vector2i)->tmp_cell_class:
+		return tmp_cell_class.new(); 
+	func place_plant(cord:Vector2i, plant:Plant):
+		pass;
+
 var _StageIndex: int;
 var _Pos: Vector2i;
+var _Map: tmp_map_class;
+
+var currentTile: Vector2i;
+var stages: Array[Vector2i];
 
 func grow()->void:
 	pass
 
 #set a plants growth stage
-func set_stage(StageIndex: int)->bool:#needs to be able to set tiles via map
+func _SetStage(StageIndex: int)->bool:
+	if StageIndex in range(0, stages.size()):
+		_StageIndex = StageIndex;
+		currentTile = stages[StageIndex];
+		return true;
 	return false;
 
 #create a new plant: call this function on the subclass you want, not Plant
-static func create_plant(Pos: Vector2i, StageIndex: int)->Plant:
+static func create_plant(Pos: Vector2i, Map: tmp_map_class, StageIndex: int)->Plant:
 	assert(false, "call createPlant on a subclass of plant, not the base class");
 	return null;
