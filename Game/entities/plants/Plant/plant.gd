7438f10e3aed46fd90b4df5fba148270123bb8e9
aidansterling6 <95391945+aidansterling6@users.noreply.github.com>
2024-11-21 17:52:14 -0800

    update plant class and added wheat subclass

--- Game/entities/plants/Plant/plant.gd
+++ Game/entities/plants/Plant/plant.gd
@@ -1,36 +1,22 @@
-#extends Node2D
 class_name Plant
-
-class tmp_cell_class:
-	var sunlight: int;
-	var water: int;
-	var plant: Plant;
-
-class tmp_map_class:
-	func get_cell(cord:Vector2i)->tmp_cell_class:
-		return tmp_cell_class.new(); 
-	func place_plant(cord:Vector2i, plant:Plant):
-		pass;
 
 var _StageIndex: int;
 var _Pos: Vector2i;
-var _Map: tmp_map_class;
+var _Map: Map;
 
 var currentTile: Vector2i;
-var stages: Array[Vector2i];
+var _Stages: Array[Vector2i];
 
 func grow()->void:
 	pass
 
 #set a plants growth stage
 func _SetStage(StageIndex: int)->bool:
-	if StageIndex in range(0, stages.size()):
+	if StageIndex in range(0, _Stages.size()):
 		_StageIndex = StageIndex;
-		currentTile = stages[StageIndex];
+		currentTile = _Stages[StageIndex];
 		return true;
 	return false;
-
-#create a new plant: call this function on the subclass you want, not Plant
-static func create_plant(Pos: Vector2i, Map: tmp_map_class, StageIndex: int)->Plant:
-	assert(false, "call createPlant on a subclass of plant, not the base class");
-	return null;
+	
+func _init():
+	assert(false, "don't create a Plant object, create an object of one of it's subclasses such as flower");
--- Game/entities/plants/Wheat/wheat.gd
+++ Game/entities/plants/Wheat/wheat.gd
@@ -1,16 +1,16 @@
 extends Plant
-class_name Flower
+class_name Wheat
 
 func grow()->void:
 	var tmp_sun = _Map.get_cell_sunlight(_Pos);
 	var tmp_water = _Map.get_cell_water(_Pos);
-	if tmp_sun > 3 and tmp_water > 3 and _StageIndex + 1 <= 2:
+	if tmp_sun > 2 and tmp_water > 2 and _StageIndex + 1 <= 2:
 		_SetStage(_StageIndex + 1);
-	elif tmp_sun < 1 or tmp_water < 1:
+	elif tmp_sun < 0 or tmp_water < 0:
 		_SetStage(0);
 
 func _init(Pos: Vector2i, MapInstance: Map):
-	_Stages = [Vector2i(8, 0), Vector2i(0, 1), Vector2i(1, 1)];
+	_Stages = [Vector2i(10, 12), Vector2i(10, 13), Vector2i(10, 14)];
 	_Pos = Pos;
 	_StageIndex = 0;
 	_Map = MapInstance;
