fe5d6eace6e7e018be8c48dac75d312fc498abee
jaxonRuiz <45025385+jaxonRuiz@users.noreply.github.com>
2024-12-07 16:39:58 -0800

    added comments

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,17 +1,18 @@
-// src/i18n.js
 import { addMessages, init, getLocaleFromNavigator } from 'svelte-i18n';
 
+// import translation packs 
 import en from './locale/en.json';
 import es from './locale/es.json';
 import zh from './locale/zh.json';
 import ar from './locale/ar.json';
 
-
+// add translations to app
 addMessages('en', en);
 addMessages('es', es);
 addMessages('zh', zh);
 addMessages('ar', ar);
 
+// initialize i18n and language
 init({
   fallbackLocale: 'en',
   initialLocale: getLocaleFromNavigator(),
